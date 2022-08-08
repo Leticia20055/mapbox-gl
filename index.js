31cf45f155d4a602c5608d8a6ef14b2be478d6cf
Leticia Miranda <leticiamirandat20@gmail.com>
2022-08-08 07:53:11 -0600

    Comentario

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const mapboxgl = require('mapbox-gl')
 const map = require('./scripts/map')
-
+    //ewjfhghjvjhervjvrfjvrv
 mapboxgl.accessToken =
-	'pk.eyJ1Ijoic2VybGlzIiwiYSI6ImNsNXNyYjc2ODA1OXkzYnFxYTY2Y3piYXcifQ.YBHbD2NsbJ3rs65L5Wfbqw'
+    'pk.eyJ1Ijoic2VybGlzIiwiYSI6ImNsNXNyYjc2ODA1OXkzYnFxYTY2Y3piYXcifQ.YBHbD2NsbJ3rs65L5Wfbqw'
 
 app.set('view engine', 'ejs')
 app.use(express.static('dist'))
@@ -12,10 +12,10 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-	res.setHeader('Access-Control-Allow-Origin', '*')
-	res.render('index', { tokenID: mapboxgl.accessToken })
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.render('index', { tokenID: mapboxgl.accessToken })
 })
 
 app.listen(3000, () => {
-	console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!')
 })
